18498aedb4adb55cde234a0e1d32b61db4992e4e
Brig Bagley <brig@sci.utah.edu>
2015-10-14 17:47:24 -0600

    builds un linux

--- src/core3d/cuda/meshFIM.cu
+++ src/core3d/cuda/meshFIM.cu
@@ -10,6 +10,7 @@
 #include <cusp/detail/format_utils.h>
 #include <cusp/print.h>
 #include <thrust/functional.h>
+#include <sstream>
 
 extern "C"
 {
@@ -50,7 +51,9 @@ void meshFIM::writeVTK(std::vector < std::vector <LevelsetValueType> > values)
   int nt = m_meshPtr->tets.size();
   for (size_t j = 0; j < values.size(); j++) {
     FILE* vtkfile;
-    vtkfile = fopen((std::string("result") + std::to_string(j) + std::string(".vtk")).c_str(), "w+");
+    std::stringstream ss;
+    ss << "result" << j << ".vtk";
+    vtkfile = fopen(ss.str().c_str(), "w+");
     fprintf(vtkfile, "# vtk DataFile Version 3.0\nvtk output\nASCII\nDATASET UNSTRUCTURED_GRID\n");
     fprintf(vtkfile, "POINTS %d float\n", nv);
     for (int i = 0; i < nv; i++)

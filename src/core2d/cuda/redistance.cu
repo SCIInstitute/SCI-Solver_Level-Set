c4cb488b71cf4400021ba8c2ec30bd0339cd43b8
Brig Bagley <brig@sci.utah.edu>
2015-10-19 11:12:54 -0600

    no build warnings on linux

--- src/core2d/cuda/redistance.cu
+++ src/core2d/cuda/redistance.cu
@@ -8,9 +8,9 @@
 #include "cusp/print.h"
 
 void redistance::ReInitTsign(TriMesh* mesh, Vector_d& vertT_after_permute_d, int nparts, int largest_vert_part, int largest_ele_part, int largest_num_inside_mem, int full_num_ele,
-                             Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
-                             IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d, IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
-                             IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy)
+    Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
+    IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d, IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
+    IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy)
 {
   int nn = mesh->vertices.size();
   int nthreads = 256;
@@ -19,11 +19,11 @@ void redistance::ReInitTsign(TriMesh* mesh, Vector_d& vertT_after_permute_d, int
 }
 
 void redistance::FindSeedPoint(const IdxVector_d& old_narrowband, const int num_old_narrowband, TriMesh* mesh, Vector_d& vertT_after_permute_d, int nparts,
-  int largest_vert_part, int largest_ele_part, int largest_num_inside_mem, int full_num_ele,
-  Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
-  IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d,
-  IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
-  IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy)
+    int largest_vert_part, int largest_ele_part, int largest_num_inside_mem, int full_num_ele,
+    Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
+    IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d,
+    IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
+    IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy)
 {
   int ne = mesh->faces.size();
   int nn = mesh->vertices.size();
@@ -37,14 +37,14 @@ void redistance::FindSeedPoint(const IdxVector_d& old_narrowband, const int num_
     int nblocks = nparts;
 
     cudaSafeCall((kernel_seedlabel << <nblocks, nthreads >> >(nn, full_num_ele,
-      CAST(vert_after_permute_d),
-      CAST(vert_offsets_d),
-      CAST(ele_after_permute_d),
-      CAST(ele_offsets_d),
-      CAST(m_Label_d),
-      CAST(vertT_after_permute_d),
-      CAST(m_DT_d),
-      CAST(m_active_block_list_d))));
+            CAST(vert_after_permute_d),
+            CAST(vert_offsets_d),
+            CAST(ele_after_permute_d),
+            CAST(ele_offsets_d),
+            CAST(m_Label_d),
+            CAST(vertT_after_permute_d),
+            CAST(m_DT_d),
+            CAST(m_active_block_list_d))));
   }
   else
   {
@@ -52,24 +52,24 @@ void redistance::FindSeedPoint(const IdxVector_d& old_narrowband, const int num_
     int nthreads = largest_ele_part;
     int nblocks = nnb;
     cudaSafeCall((kernel_seedlabel_narrowband << <nblocks, nthreads >> >(nn, full_num_ele, CAST(old_narrowband),
-      CAST(vert_after_permute_d),
-      CAST(vert_offsets_d),
-      CAST(ele_after_permute_d),
-      CAST(ele_offsets_d),
-      CAST(m_Label_d),
-      CAST(vertT_after_permute_d),
-      CAST(m_DT_d),
-      CAST(m_active_block_list_d))));
+            CAST(vert_after_permute_d),
+            CAST(vert_offsets_d),
+            CAST(ele_after_permute_d),
+            CAST(ele_offsets_d),
+            CAST(m_Label_d),
+            CAST(vertT_after_permute_d),
+            CAST(m_DT_d),
+            CAST(m_active_block_list_d))));
 
   }
 }
 
 void redistance::GenerateData(IdxVector_d& new_narrowband, int& new_num_narrowband, LevelsetValueType bandwidth, int stepcount, TriMesh* mesh, Vector_d& vertT_after_permute_d,
-                              int nparts, int largest_vert_part, int largest_ele_part, int largest_num_inside_mem, int full_num_ele,
-                              Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
-                              IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d,
-                              IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
-                              IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy, bool verbose)
+    int nparts, int largest_vert_part, int largest_ele_part, int largest_num_inside_mem, int full_num_ele,
+    Vector_d& vert_after_permute_d, IdxVector_d& vert_offsets_d,
+    IdxVector_d& ele_after_permute_d, IdxVector_d& ele_offsets_d, Vector_d& ele_local_coords_d,
+    IdxVector_d& mem_location_offsets, IdxVector_d& mem_locations,
+    IdxVector_d& part_label_d, IdxVector_d& block_xadj, IdxVector_d& block_adjncy, bool verbose)
 {
   int nn = mesh->vertices.size();
   int totalIterationNumber = 0;
@@ -95,7 +95,7 @@ void redistance::GenerateData(IdxVector_d& new_narrowband, int& new_num_narrowba
       }
       printf(" %d Active blocks.\n", numActive);
     }
-//    printf("nTotalIter = %d, numActive=%d\n", nTotalIter, numActive);
+    //    printf("nTotalIter = %d, numActive=%d\n", nTotalIter, numActive);
     ///////////////////////////step 1: run solver //////////////////////////////////////////////////////////////////
     nTotalIter++;
     totalIterationNumber += numActive;
@@ -105,39 +105,39 @@ void redistance::GenerateData(IdxVector_d& new_narrowband, int& new_num_narrowba
     m_active_block_list_d = h_ActiveList;
     shared_size = sizeof(LevelsetValueType)* 3 * largest_ele_part + sizeof(short)*largest_vert_part*largest_num_inside_mem;
     cudaSafeCall((kernel_update_values << <nblocks, nthreads, shared_size >> >(CAST(m_active_block_list_d), CAST(m_Label_d), largest_ele_part, largest_vert_part, full_num_ele,
-                                                                               CAST(ele_after_permute_d), CAST(ele_offsets_d),
-                                                                               CAST(vert_offsets_d), CAST(m_DT_d),
-                                                                               CAST(ele_local_coords_d), largest_num_inside_mem, CAST(mem_locations), CAST(mem_location_offsets),
-                                                                               NUM_ITER, CAST(DT_d_out), CAST(d_vert_con))));
+            CAST(ele_after_permute_d), CAST(ele_offsets_d),
+            CAST(vert_offsets_d), CAST(m_DT_d),
+            CAST(ele_local_coords_d), largest_num_inside_mem, CAST(mem_locations), CAST(mem_location_offsets),
+            NUM_ITER, CAST(DT_d_out), CAST(d_vert_con))));
     nthreads = largest_vert_part;
     cudaSafeCall((CopyOutBack << <nblocks, nthreads >> >(CAST(m_active_block_list_d),
-                                                         CAST(vert_offsets_d), CAST(m_DT_d), CAST(DT_d_out))));
+            CAST(vert_offsets_d), CAST(m_DT_d), CAST(DT_d_out))));
 
     //////////////////////step 2: reduction////////////////////////////////////////////////
     if(nthreads <= 32)
     {
       cudaSafeCall((run_reduction_bandwidth < 32 > << <nblocks, 32 >> > (CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d),
-                                                                         CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
+              CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
     }
     else if(nthreads <= 64)
     {
       cudaSafeCall((run_reduction_bandwidth < 64 > << <nblocks, 64 >> > (CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d),
-                                                                         CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
+              CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
     }
     else if(nthreads <= 128)
     {
       cudaSafeCall((run_reduction_bandwidth < 128 > << <nblocks, 128 >> > (CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d),
-                                                                           CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
+              CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
     }
     else if(nthreads <= 256)
     {
       cudaSafeCall((run_reduction_bandwidth < 256 > << <nblocks, 256 >> > (CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d),
-                                                                           CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
+              CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
     }
     else if(nthreads <= 512)
     {
       cudaSafeCall((run_reduction_bandwidth < 512 > << <nblocks, 512 >> > (CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d),
-                                                                           CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
+              CAST(DT_d_out), CAST(d_block_vertT_min), CAST(vert_offsets_d))));
     }
     else
     {
@@ -200,22 +200,25 @@ void redistance::GenerateData(IdxVector_d& new_narrowband, int& new_num_narrowba
 
       int sharedSize = sizeof(LevelsetValueType)* 3 * largest_ele_part + sizeof(short)*largest_vert_part*largest_num_inside_mem;
       cudaSafeCall((kernel_run_check_neghbor << <nblocks, nthreads, shared_size >> >(CAST(m_active_block_list_d), CAST(m_Label_d), largest_ele_part, largest_vert_part,
-                                                                                     full_num_ele,
-                                                                                     CAST(ele_after_permute_d), CAST(ele_offsets_d),
-                                                                                     CAST(vert_offsets_d), CAST(m_DT_d),
-                                                                                     CAST(ele_local_coords_d), largest_num_inside_mem, CAST(mem_locations), CAST(mem_location_offsets), 1,
-                                                                                     CAST(DT_d_out), CAST(d_vert_con))));
+              full_num_ele,
+              CAST(ele_after_permute_d), CAST(ele_offsets_d),
+              CAST(vert_offsets_d), CAST(m_DT_d),
+              CAST(ele_local_coords_d), largest_num_inside_mem, CAST(mem_locations), CAST(mem_location_offsets), 1,
+              CAST(DT_d_out), CAST(d_vert_con))));
+      if (sharedSize <= 0) {
+        printf("Error: zero shared size");
+      }
 
 
       ////////////////////////////////////////////////////////////////
-      // 5. reduction 
+      // 5. reduction
       ////////////////////////////////////////////////////////////////
       nthreads = largest_vert_part;
       run_reduction << <nblocks, nthreads >> >(CAST(d_vert_con), CAST(d_block_con), CAST(m_active_block_list_d), CAST(vert_offsets_d));
 
       //////////////////////////////////////////////////////////////////
       // 6. update active list
-      // read back active volume from the device and add 
+      // read back active volume from the device and add
       // active block to active list on the host memory
       h_block_con = d_block_con;
       for(int i = 0; i < h_ActiveListNew.size(); i++)

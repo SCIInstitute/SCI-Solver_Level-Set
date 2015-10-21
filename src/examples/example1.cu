6ac515521c179138bcd87c579d0b333d27501e7d
Brig Bagley <brig@sci.utah.edu>
2015-10-21 17:08:03 -0600

    better 3d sphere expansion

--- src/examples/example1.cu
+++ src/examples/example1.cu
@@ -97,6 +97,7 @@ int main(int argc, char *argv[])
         LevelSet3d::mesh_->vertices[LevelSet3d::mesh_->tets[i][2]])
         / 3. - center;
       double mag = std::sqrt(p[0] * p[0] + p[1] * p[1] + p[2] * p[2]);
+      mag /= max / 20.;
       adv.push_back(p / mag / mag);
     }
     LevelSet3d::initializeVertices(data, vals);
